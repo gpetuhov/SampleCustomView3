99b66dc633c68a484871cd09ec8a29179907df86
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-22 16:34:50 +0300

    Add rotation

--- app/src/main/java/com/gpetuhov/android/samplecustomview3/MyCustomView.kt
+++ app/src/main/java/com/gpetuhov/android/samplecustomview3/MyCustomView.kt
@@ -10,12 +10,21 @@ import androidx.annotation.RequiresApi
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Paint.ANTI_ALIAS_FLAG
+import android.animation.PropertyValuesHolder
+
+
 
 
 class MyCustomView : View {
 
+    companion object {
+        const val PROPERTY_RADIUS = "propertyRadius"
+        const val PROPERTY_ROTATE = "propertyRotate"
+    }
+
     private var paint = Paint(ANTI_ALIAS_FLAG)
     private var radius = 0
+    private var rotate = 0
     private var animator: ValueAnimator? = null
 
     constructor(context: Context) : super(context) {
@@ -55,6 +64,13 @@ class MyCustomView : View {
         val rightBotX = viewWidth + 150
         val rightBotY = viewHeight + 150
 
+        // Rotate canvas before drawing
+        canvas?.rotate(
+            rotate.toFloat(),
+            viewWidth.toFloat(),
+            viewHeight.toFloat()
+        )
+
         canvas?.drawRoundRect(
             leftTopX.toFloat(),
             leftTopY.toFloat(),
@@ -72,10 +88,22 @@ class MyCustomView : View {
             return
         }
 
-        animator = ValueAnimator.ofInt(0, 150)
+        // One animator can animate more than one property by using PropertyValuesHolder
+        val propertyRadius = PropertyValuesHolder.ofInt(PROPERTY_RADIUS, 0, 150)
+        val propertyRotate = PropertyValuesHolder.ofInt(PROPERTY_ROTATE, 0, 360)
+
+        animator = ValueAnimator()
+
+        // Here we set animater to animate 2 properties
+        animator?.setValues(propertyRadius, propertyRotate)
+
         animator?.duration = 2000
         animator?.addUpdateListener { animation ->
-            radius = animation.animatedValue as Int
+            // Get current value of the animated properties
+            radius = animation.getAnimatedValue(PROPERTY_RADIUS) as Int
+            rotate = animation.getAnimatedValue(PROPERTY_ROTATE) as Int
+
+            // Invalidate view to update it with new radius and rotate values
             invalidate()
         }
         animator?.start()
